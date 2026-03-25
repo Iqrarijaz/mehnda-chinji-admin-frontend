65e7a2265cab6a92b12ed099a1a4c2bc27d4702b
iqrarijaz788 <iqrarijaz788@gmail.com>
2026-03-25 17:48:52 +0500

    code

---
+++ components/providers/ClientProviders.jsx
@@ -0,0 +1,47 @@
+"use client";
+import React, { useState } from "react";
+import { ConfigProvider } from "antd";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { ToastContainer } from "react-toastify";
+import MenuContextProvider from "@/context/MenuContext";
+
+export default function ClientProviders({ children }) {
+  const [queryClient] = useState(() => new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  }));
+
+  return (
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#0F172A",
+          borderRadius: 5,
+          colorBgContainer: "#ffffff",
+          fontFamily: "Poppins, sans-serif",
+          fontSize: 16,
+          colorText: "#404040",
+        },
+      }}
+    >
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <ToastContainer
+          position="top-right"
+          autoClose={3500}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          pauseOnHover
+          draggable={false}
+          theme="light"
+        />
+        <MenuContextProvider>{children}</MenuContextProvider>
+      </QueryClientProvider>
+    </ConfigProvider>
+  );
+}
