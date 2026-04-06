847397519f6febed54f6caed6f3c2b40dc362c9e
iqrar-ijaz <iqrar.ijaz@axabiztech.com>
2026-04-06 12:13:20 +0500

    udated .env

--- config/index.jsx
+++ config/index.jsx
@@ -1,7 +1,7 @@
 // env file config
 // base urls
-// export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:5000";
+export const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:5000";
 
-export const baseUrl = "http://localhost:5000";
+// export const baseUrl = "http://localhost:5000";
 
 // export const baseUrl = "https://api.mehndachinji.com"
